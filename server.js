a89c9d49c2b8b5d7a494d3b513fd882ea36b496f
sarthak19-qait <sarthaksaxena@qainfotech.com>
2019-06-26 12:47:56 +0530

    Prepare for heroku

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/dist'));
+
+app.all('*',(req,res)=>{
+    res.status(200).sendFile(_-__dirname+'/dist/index.html')
+});
+app.listen(process.env.PORT || 8080);
